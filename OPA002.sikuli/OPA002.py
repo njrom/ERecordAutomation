be91424ccc1df3e0a20e72d0bf0983768f5a9d70
Romano <nromano@urmc-sh.rochester.edu>
2018-03-30 15:16:27 -0400

    Scrolls to Patient Insurance Info in the c11 form

--- OPA002.sikuli/OPA002.py
+++ OPA002.sikuli/OPA002.py
@@ -19,11 +19,6 @@ def main():
     type("ORMD" + Key.TAB)
     type("t+5")
     click("1522354668981.png") # New case button
-
-
-def test():
-    switchApp("Hyperspace")
-    
     wait(2)
     type("Inpatient")  # Patient Class
     type(Key.TAB + Key.TAB + Key.TAB)
@@ -31,33 +26,51 @@ def test():
     type("SMH MAIN OR")
     type(Key.TAB + Key.TAB + Key.TAB)
     type("P05.00" + Key.ENTER + Key.ENTER)  # Diagnosis
-    click(Pattern("1522191047332.png").targetOffset(0, 15))  # Clicks box to ender p
+    click(Pattern("1522436563595.png").targetOffset(0, 15))  # Clicks box to ender p
     type("Craniotomy for Aneurysm")
     click(Pattern("1522191069162.png").targetOffset(0, 15))
     type("N/A")  # Laterality
     click(Pattern("1522354869064.png").targetOffset(0, 15))
     type("General")  # Anesthesia
     click("1522356679271.png") # Case Entry Form
+   
+
+
+def test():
+    switchApp("Hyperspace")
     wait(4)
+    while(not exists("1522436664179.png")):
+        wait(1) # Patient Information Header Check to see if page is loaded
     click("1522356903531.png")
     type(Key.TAB+Key.ENTER) # Hits Yes on PCP Listed
     click("1522357884191.png") # Address and Phone
     type(Key.TAB+Key.ENTER)
-    click(Pattern().targetOffset(0,15))
+    click(Pattern("1522367277299.png").targetOffset(0,15))
     type("Yes"+Key.TAB)
     type("Spanish Interpreter")
     click("1522358364237.png") # Latex Allergy
     type(Key.TAB+Key.TAB+Key.ENTER)
-    click("1522358721822.png" # Dye Allergy 
-    type(Key.TAB+Key.ENTER) # NO
-    type("1522359580669.png")
-    while(!exists("1522359580669.png")):
-            type(Key.TAB)
-            wait(1)
-    click("1522359580669.png")
+    click("1522358721822.png") # Dye Allergy 
+    type(Key.TAB+Key.ENTER)
+    click("1522372124942.png")
+    type(Key.TAB*15+Key.ENTER) # Selecteds NIDDM, This is a major limitation of the code
+    while(not exists("1522372377542.png")):
+        type(Key.TAB*15)
+    type(Key.TAB*7) 
+    click(Pattern("1522372377542.png").targetOffset(0,15))
+    type("T+3") # Procedure Date
+    type(Key.TAB+"2") # Days staying
+    while(not exists("1522436964040.png")):
+        type(Key.TAB*15) # Scroll till you see the Patient Insurance header
+    type(Key.TAB*7)
+    wait(1)
+    click("1522437004745.png")
+    type(Key.TAB+Key.ENTER)
     
     
     
 
-test()    
+    
+   
+test()  
     
